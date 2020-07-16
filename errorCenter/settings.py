582a6431870bcb678a4a098bd6a2b74aac5af0a9
Matheus Mendes <ronaldo_rogerio@yahoo.com.br>
2020-07-15 22:32:04 -0300

    template correction

--- errorCenter/settings.py
+++ errorCenter/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'errorCenter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'center/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
